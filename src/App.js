58c696c5c014833db415ef971c2e9935cd81f654
Sam O'Cain <samocain93@gmail.com>
2023-06-21 10:05:08 -0500

    basic functionality working and bill and tip working

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import './styles.css';
+
+export default function App() {
+  return (
+    <div className='App'>
+      <TipCalculator />
+    </div>
+  );
+}
+
+function TipCalculator() {
+  const [bill, setBill] = useState('');
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
+
+  return (
+    <div className='calculator'>
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPercentage percentage={percentage1} onSelect={setPercentage1}>
+        How did you like the service?
+      </SelectPercentage>
+      <SelectPercentage percentage={percentage2} onSelect={setPercentage2}>
+        How did your friend like the service?
+      </SelectPercentage>
+      <Output bill={bill} tip={tip} />
+      <Reset />
+    </div>
+  );
+}
+
+function BillInput({ bill, onSetBill }) {
+  return (
+    <div className='bill'>
+      <label>How much was the total bill? </label>
+      <input
+        type='text'
+        placeholder='(Total in $)'
+        value={bill}
+        onChange={(e) => onSetBill(Number(e.target.value))}
+      ></input>
+    </div>
+  );
+}
+
+function SelectPercentage({ children, percentage, onSelect }) {
+  return (
+    <div className='percentage'>
+      <label>{children}</label>
+      <select value={percentage} onChange={(e) => onSelect(Number(e.target.value))}>
+        <option value='10'>Terrible (10%)</option>
+        <option value='15'>Good (15%)</option>
+        <option value='20'>Awesome! (20%)</option>
+        <option value='25'>OUTSTANDING! (25%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Output({ bill, tip }) {
+  return <h3>You pay ${bill + tip} (${bill} + $ {tip})</h3>;
+}
+
+function Reset() {
+  return <button>Reset</button>;
+}
